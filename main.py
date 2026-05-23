b212b3f561e4ebb2d0a0cd56d628a1124b3c5e52
iinayaturrizqa-art <iinayaturrizqa@gmail.com>
2026-05-23 15:30:22 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ PROMO_TEXT = (
 "🛡️ Praktis & mudah digunakan\n"
 "⏰ Admin standby 7/24 jam\n\n"
 "🚀 **Order langsung via bot**\n"
-"👉 @GlobalOTPhubBot"
+"👉 @farinshop_bot"
 )
 
 status_msg_id = None
